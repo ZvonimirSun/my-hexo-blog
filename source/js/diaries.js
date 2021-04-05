65befdd9208222c03c88ef3308d1c7996fc9fc3a
ZvonimirSun <hi@iszy.xyz>
2021-04-05 22:00:12 +0800

    update

--- source/js/diaries.js
+++ source/js/diaries.js
@@ -1,7 +1,7 @@
 function showDetail(e) {
-  if (e.classList.contains("show")) {
-    e.classList.remove("show");
-  } else {
-    e.classList.add("show");
+  let tmp = document.getElementsByClassName("diary");
+  for (const dom of tmp) {
+    dom.classList.add("show");
   }
+  e.classList.remove("show");
 }
