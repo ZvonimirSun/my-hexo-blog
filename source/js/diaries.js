e9625dca8e0ac3f02d072fefa6777ef43a6f46ba
ZvonimirSun <hi@iszy.xyz>
2021-04-05 17:50:27 +0800

    update

--- source/js/diaries.js
+++ source/js/diaries.js
@@ -3,9 +3,9 @@ for (const dom of tmp) {
   dom.addEventListener(
     "click",
     function (e) {
-      if (e && e.target && e.target.classList.includes("diary-content")) {
+      if (e && e.target && e.target.classList.contains("diary-content")) {
         let d = e.target;
-        if (d.classList.includes("show")) {
+        if (d.classList.contains("show")) {
           d.classList.remove("show");
         } else {
           d.classList.add("show");
