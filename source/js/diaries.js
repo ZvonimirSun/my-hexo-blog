b214dc9d250cd159a05e421fb3311e3e60ad0916
ZvonimirSun <hi@iszy.xyz>
2021-04-05 17:48:01 +0800

    update

--- source/js/diaries.js
+++ source/js/diaries.js
@@ -1,11 +1,17 @@
-document.getElementsByClassName("diary-content").addEventListener(
-  "click",
-  function (e) {
-    if (e.classList.includes("show")) {
-      e.classList.remove("show");
-    } else {
-      e.classList.add("show");
-    }
-  },
-  false
-);
+let tmp = document.getElementsByClassName("diary-content");
+for (const dom of tmp) {
+  dom.addEventListener(
+    "click",
+    function (e) {
+      if (e && e.target && e.target.classList.includes("diary-content")) {
+        let d = e.target;
+        if (d.classList.includes("show")) {
+          d.classList.remove("show");
+        } else {
+          d.classList.add("show");
+        }
+      }
+    },
+    false
+  );
+}
